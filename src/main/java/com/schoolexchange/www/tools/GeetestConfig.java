a569a96035ed91c971ed4f9299c7e1a60100cdf1
zhangjiadong <zhangjiadong0418@qq.com>
2015-12-03 22:34:20 +0800

    geetest验证

---
+++ src/main/java/com/schoolexchange/www/tools/GeetestConfig.java
@@ -0,0 +1,22 @@
+package com.schoolexchange.www.tools;
+
+/**
+ * GeetestWeb配置文件
+ * 
+ * @author zheng
+ *
+ */
+public class GeetestConfig {
+
+	private static final String captcha_id = "b46d1900d0a894591916ea94ea91bd2c";
+	private static final String private_key = "36fc3fe98530eea08dfc6ce76e3d24c4";
+
+	public static final String getCaptcha_id() {
+		return captcha_id;
+	}
+
+	public static final String getPrivate_key() {
+		return private_key;
+	}
+
+}
