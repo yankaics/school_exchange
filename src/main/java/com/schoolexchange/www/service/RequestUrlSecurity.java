79f698b908acfc2f6fe1dd58446075dfa186c3bc
zhangjiadong <zhangjiadong0418@qq.com>
2015-12-05 15:35:36 +0800

    加密

---
+++ src/main/java/com/schoolexchange/www/service/RequestUrlSecurity.java
@@ -0,0 +1,76 @@
+package com.schoolexchange.www.service;
+
+import java.security.Key;
+
+/**
+ * Created by Administrator
+ * on 2015/12/5
+ */
+public interface RequestUrlSecurity {
+    /**
+     * 将byte数组转换为表示16进制值的字符串， 如：byte[]{8,18}转换为：0813， 和public static byte[]
+     * hexStr2ByteArr(String strIn) 互为可逆的转换过程
+     *
+     * @param arrB 需要转换的byte数组
+     * @return 转换后的字符串
+     * @throws Exception 本方法不处理任何异常，所有异常全部抛出
+     */
+    String byteArr2HexStr(byte[] arrB) throws Exception;
+
+    /**
+     * 将表示16进制值的字符串转换为byte数组， 和public static String byteArr2HexStr(byte[] arrB)
+     * 互为可逆的转换过程
+     *
+     * @param strIn 需要转换的字符串
+     * @return 转换后的byte数组
+     * @throws Exception 本方法不处理任何异常，所有异常全部抛出
+     * @author <a href="mailto:leo841001@163.com">LiGuoQing</a>
+     */
+    byte[] hexStr2ByteArr(String strIn) throws Exception;
+
+
+    /**
+     * 加密字节数组
+     *
+     * @param arrB 需加密的字节数组
+     * @return 加密后的字节数组
+     * @throws Exception
+     */
+    byte[] encrypt(byte[] arrB) throws Exception;
+
+    /**
+     * 加密字符串
+     *
+     * @param strIn 需加密的字符串
+     * @return 加密后的字符串
+     * @throws Exception
+     */
+    String encrypt(String strIn) throws Exception;
+
+    /**
+     * 解密字节数组
+     *
+     * @param arrB 需解密的字节数组
+     * @return 解密后的字节数组
+     * @throws Exception
+     */
+    byte[] decrypt(byte[] arrB) throws Exception;
+
+    /**
+     * 解密字符串
+     *
+     * @param strIn 需解密的字符串
+     * @return 解密后的字符串
+     * @throws Exception
+     */
+    String decrypt(String strIn) throws Exception;
+
+    /**
+     * 从指定字符串生成密钥，密钥所需的字节数组长度为8位 不足8位时后面补0，超出8位只取前8位
+     *
+     * @param arrBTmp 构成该字符串的字节数组
+     * @return 生成的密钥
+     * @throws java.lang.Exception
+     */
+    Key getKey(byte[] arrBTmp) throws Exception;
+}
